6be1ad86cd59065c632eec027dc56829d58a8f7e
superbasile <basile2710@gmail.com>
2024-05-21 11:12:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const songs = [
-    { src: "https://youtu.be/C-u5WLJ9Yk4", answer: "Baby one more time" },
-    { src: "https://youtu.be/LOZuxwVk7TU", answer: "Toxic" },
+    { src: "https://www.youtube.com/watch?v=VIDEO_ID_1", answer: "Song Title 1" },
+    { src: "https://www.youtube.com/watch?v=VIDEO_ID_2", answer: "Song Title 2" },
     // Ajoutez plus de chansons ici
 ];
 
+let player;
 let currentSongIndex = 0;
 let score = 0;
 
-const audioElement = document.getElementById('audio');
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '200',
+        width: '300',
+        videoId: '',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    loadSong(currentSongIndex);
+}
+
 const playButton = document.getElementById('playButton');
 const answerInput = document.getElementById('answerInput');
 const submitAnswerButton = document.getElementById('submitAnswer');
@@ -20,14 +35,12 @@ function loadSong(index) {
         document.getElementById('game').style.display = 'none';
         return;
     }
-    // Utilisation de l'API de YouTube pour intégrer la vidéo
     const videoId = new URL(songs[index].src).searchParams.get('v');
-    const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&controls=1`;
-    audioElement.src = embedUrl;
+    player.loadVideoById(videoId);
 }
 
 playButton.addEventListener('click', () => {
-    audioElement.src += '&autoplay=1';
+    player.playVideo();
 });
 
 submitAnswerButton.addEventListener('click', () => {
@@ -44,5 +57,3 @@ submitAnswerButton.addEventListener('click', () => {
     currentSongIndex++;
     loadSong(currentSongIndex);
 });
-
-loadSong(currentSongIndex);
